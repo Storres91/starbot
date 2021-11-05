0144610651c6404efc71abcadae6ab297151e752
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-04 23:31:23 -0500

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -3,20 +3,19 @@
 module.exports = {
     execute(message, client, Discord) {
         const prefix = 'sb ';
-        if (message.content.toLowerCase().startsWith(prefix)) {
-            if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
-            if (message.channel.type == 'dm') return;
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+        if (message.channel.type == 'dm') return;
 
-            const args = message.content.toLowerCase().slice(prefix.length).split(/ +/);
-            const cmd = args.shift().toLowerCase();
+        const args = message.content.toLowerCase().slice(prefix.length).split(/ +/);
+        const cmd = args.shift().toLowerCase();
 
-            const command = client.commands.get(cmd);
+        const command = client.commands.get(cmd);
 
-            try {
-                if (command) command.execute(client, message, args, Discord);
-            } catch (err) {
-                console.log(err);
-            }
+        try {
+            if (command) command.execute(client, message, args, Discord);
+        } catch (err) {
+            console.log(err);
+
         }
     }
 }
