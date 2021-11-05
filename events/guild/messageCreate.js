8af6edfefec18ccd33e99c9d8fbf623f37ffb28d
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-04 23:16:08 -0500

    prefix test

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -2,13 +2,12 @@
 
 module.exports = {
     execute(message, client, Discord) {
-        message.content.toLowerCase();
         const prefix = 'sb ';
         
             if (!message.content.startsWith(prefix) || message.author.bot) return;
             if (message.channel.type == 'dm') return;
-
-            const args = message.content.slice(prefix.length).split(/ +/);
+           
+            const args = message.content.slice(prefix.length).split(/ +/).toLowerCase();
             const cmd = args.shift().toLowerCase();
 
             const command = client.commands.get(cmd);
--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -3,13 +3,6 @@ const fs = require('fs');
 module.exports = (client, Discord, message) => {
     const load_dir = (dir) => {
         const event_files = fs.readdirSync(`./events/${dir}/`).filter(file => file.endsWith('.js'));
-
-        // for (const file of event_files) {
-        //     const event = require(`../events/${dir}/${file}`);
-        //     const event_name = file.split('.')[0];
-        //     client.on(event_name, event.bind(null, client, Discord));
-            
-        // }
         for (const file of event_files) {
             const event = require(`../events/${dir}/${file}`);
             const event_name = file.split('.')[0];        
