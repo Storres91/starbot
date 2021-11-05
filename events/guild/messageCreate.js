a1ebc77fa6d13466fb1d98efbf4a4a0954fa83e6
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-04 23:01:26 -0500

    Change name message > messageCreate
    make prefix non case sensitive

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -2,7 +2,9 @@
 
 module.exports = {
     execute(message, client, Discord) {
+        
         const prefix = 'sb ';
+        if (message.content.toLowerCase().startsWith(prefix)) {
             if (!message.content.startsWith(prefix) || message.author.bot) return;
             if (message.channel.type == 'dm') return;
 
@@ -16,6 +18,6 @@ module.exports = {
             } catch (err) {
                 console.log(err);
             }
-        
+        }
     }
 }
