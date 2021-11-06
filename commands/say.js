fd3ee901938b8db22890ac53f7b7c4ce15c06439
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-06 15:01:46 -0500

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'say',
+    description: 'makes the bot say smth',
+    execute(client, message, args){
+        const messageTxt = args.join(" ");
+        if(!messageTxt) return;
+        if(message.member.user.id=='313351494361677845'){
+            setTimeout(function(){
+                message.delete();
+                message.channel.send(messageTxt);
+            },1000);
+        }
+        
+    }
+}
