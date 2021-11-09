cd6b58c6b721652a73bbb3de3a1921a2321306a7
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-09 00:42:33 -0500

    Update mbdone.js

--- commands/mbdone.js
+++ commands/mbdone.js
@@ -5,19 +5,19 @@ module.exports = {
         const mbwinnerID = '885168106383745034';
         const mbhostID = '857285753213812766';
         const staffRoleID = '857060867676831805';
-        var removedcounter = 0;
 
         if (message.member.roles.cache.some(role => role.id == mbhostID) || message.member.roles.cache.some(role => role.id == staffRoleID)) {
             const Role = message.guild.roles.cache.get(mbwinnerID);
             try {
-                Role.members.forEach((member) => { // Looping through the members of Role.
+                Role.members.forEach((member, removedcounter) => { // Looping through the members of Role.
                     setTimeout(() => {
                         member.roles.remove(Role); // Removing the Role.
 
-                    }, 13000);
-                    removedcounter = removedcounter + 1;
+                    }, 10000);
+                    message.channel.send('Succesfully removed Mb winner role from ' + String(removedcounter) + ' users')
                 });
-                message.channel.send('Succesfully removed Mb winner role removed from ' + String(removedcounter) + ' users')
+                message.channel.send('ETA: 10s')
+                
             } catch (err) {
                 message.channel.send('There was an error removing the Mb winner role, please try again')
             }
