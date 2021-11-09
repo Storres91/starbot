5b4cea1fb01953beaa21332700902993009a15fd
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-09 00:19:20 -0500

    Update mbdone.js

--- commands/mbdone.js
+++ commands/mbdone.js
@@ -5,22 +5,22 @@ module.exports = {
         const mbwinnerID = '885168106383745034';
         const mbhostID = '857285753213812766';
         const staffRoleID = '857060867676831805';
-        var removedcounter=0;
+        var removedcounter = 0;
 
-        if (message.member.roles.cache.some(role => role.id == mbhostID) || message.member.roles.cache.some(role => role.id == staffRoleID)){
-        const Role = message.guild.roles.cache.get(mbwinnerID);
-        try{
-        Role.members.forEach((member) => { // Looping through the members of Role.
-            setTimeout(() => {
-                member.roles.remove(Role); // Removing the Role.
-                
-            }, 1000);
-            removedcounter=removedcounter+1;
-        });
-        message.channel.send('Succesfully removed Mb winner role removed from '+String(removedcounter)+' users')
-        } catch(err){
-            message.channel.send('There was an error removing the Mb winner role, please try again')
-        }
-    } else return;
+        if (message.member.roles.cache.some(role => role.id == mbhostID) || message.member.roles.cache.some(role => role.id == staffRoleID)) {
+            const Role = message.guild.roles.cache.get(mbwinnerID);
+            try {
+                Role.members.forEach((member, i) => { // Looping through the members of Role.
+                    setTimeout(() => {
+                        member.roles.remove(Role); // Removing the Role.
+
+                    }, i*1000);
+                    removedcounter = removedcounter + 1;
+                });
+                message.channel.send('Succesfully removed Mb winner role removed from ' + String(removedcounter) + ' users')
+            } catch (err) {
+                message.channel.send('There was an error removing the Mb winner role, please try again')
+            }
+        } else return;
     }
 }
