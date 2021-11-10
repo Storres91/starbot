230fd639e16c266509699d7d949960e36e3a4c41
Santiago Torres <93678990+Noxet01@users.noreply.github.com>
2021-11-10 03:13:18 -0500

    Update mbdone.js

--- commands/mbdone.js
+++ commands/mbdone.js
@@ -1,27 +1,27 @@
-module.exports = {
-    name: 'mbdone',
-    description: 'Removes Mb winner role from winners',
-    async execute(client, message, args, Discord) {
-        const mbwinnerID = '885168106383745034';
-        const mbhostID = '857285753213812766';
-        const staffRoleID = '857060867676831805';
-        var removedcounter = 0;
-        if (message.member.roles.cache.some(role => role.id == mbhostID) || message.member.roles.cache.some(role => role.id == staffRoleID)) {
-            const Role =  message.guild.roles.cache.get(mbwinnerID);
-            try {
-                Role.members.forEach((member, i) => { // Looping through the members of Role.
-                    setTimeout(() => {
-                        member.roles.remove(Role); // Removing the Role.
+// module.exports = {
+//     name: 'mbdone',
+//     description: 'Removes Mb winner role from winners',
+//     async execute(client, message, args, Discord) {
+//         const mbwinnerID = '885168106383745034';
+//         const mbhostID = '857285753213812766';
+//         const staffRoleID = '857060867676831805';
+//         var removedcounter = 0;
+//         if (message.member.roles.cache.some(role => role.id == mbhostID) || message.member.roles.cache.some(role => role.id == staffRoleID)) {
+//             const Role =  message.guild.roles.cache.get(mbwinnerID);
+//             try {
+//                 Role.members.forEach((member, i) => { // Looping through the members of Role.
+//                     setTimeout(() => {
+//                         member.roles.remove(Role); // Removing the Role.
 
-                    }, i * 0.00000000000000001);
-                    removedcounter = removedcounter + 1;
-                });
-                message.channel.send('Succesfully removed Mb winner role removed from ' + String(removedcounter) + ' users')
+//                     }, i * 0.00000000000000001);
+//                     removedcounter = removedcounter + 1;
+//                 });
+//                 message.channel.send('Succesfully removed Mb winner role removed from ' + String(removedcounter) + ' users')
 
 
-            } catch (err) {
-                message.channel.send('There was an error removing the Mb winner role, please try again')
-            }
-        } else return;
-    }
-}
+//             } catch (err) {
+//                 message.channel.send('There was an error removing the Mb winner role, please try again')
+//             }
+//         } else return;
+//     }
+// }
