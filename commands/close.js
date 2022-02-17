b45e41d8186f3e5663d8cdd541229378f04c8f58
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-02-16 19:03:36 -0500

    Added confirmation of processes for reminders and gbantimers

--- commands/close.js
+++ commands/close.js
@@ -4,6 +4,6 @@ module.exports = {
     async execute(client, message, args, Discord) {
         if(message.member.nickname != null) message.channel.send(message.member.nickname+" has left the chat. <a:bg_pepobye:928148613715083334>");
         else message.channel.send(message.author.username+" has left the chat. <a:bg_pepobye:928148613715083334>");
-        message.delete().catch((err)=>client.channels.fetch().then(ch=>ch.send("```"+err+"```")))
+        message.delete().catch((err)=>client.channels.fetch("902377203801661470").then(ch=>ch.send("```"+err+"```")))
     }
 }
