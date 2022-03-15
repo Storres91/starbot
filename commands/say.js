43bff832bf2aeca137de0e676c29e0d02e4db0ee
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-15 11:45:22 -0500

    Modified say command to say exactly what u send

--- commands/say.js
+++ commands/say.js
@@ -2,12 +2,15 @@ module.exports = {
     name: 'say',
     description: 'makes the bot say smth',
     execute(client, message, args){
+        const STAFFROLE = '857060867676831805';
         const messageTxt = args.join(" ");
         if(!messageTxt) return;
-        if(message.member.user.id=='313351494361677845'){
+        if(message.member.user.id=='313351494361677845' || message.member.roles.cache.some(role => role.id == STAFFROLE)){
             setTimeout(function(){
                 message.delete().catch(() => null);
-                message.channel.send(messageTxt);
+                let messageSay = message;
+                messageSay.content.replaceAll("sb say ", "")
+                message.channel.send(messageSay);
             },1000);
         }
         
