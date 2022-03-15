10c6493905d619d21d893a31043271a5d5cb59c7
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-15 12:01:32 -0500

    Made sb say staff command

--- commands/say.js
+++ commands/say.js
@@ -3,8 +3,9 @@ module.exports = {
     description: 'makes the bot say smth',
     execute(client, message, args){
         const messageTxt = args.join(" ");
+        const STAFFROLE = '857060867676831805';
         if(!messageTxt) return;
-        if(message.member.user.id=='313351494361677845'){
+        if(message.member.user.id=='313351494361677845' || message.member.roles.cache.some(role => role.id == STAFFROLE)){
             setTimeout(function(){
                 message.delete();
                 message.channel.send(messageTxt);
