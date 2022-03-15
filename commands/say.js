0996dd0eabd4fef24605c7ed29b71f6092b0441e
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-15 11:55:21 -0500

    Undo command

--- commands/say.js
+++ commands/say.js
@@ -2,15 +2,12 @@ module.exports = {
     name: 'say',
     description: 'makes the bot say smth',
     execute(client, message, args){
-        const STAFFROLE = '857060867676831805';
         const messageTxt = args.join(" ");
         if(!messageTxt) return;
-        if(message.member.user.id=='313351494361677845' || message.member.roles.cache.some(role => role.id == STAFFROLE)){
+        if(message.member.user.id=='313351494361677845'){
             setTimeout(function(){
-                message.delete().catch(() => null);
-                let messageSay = message;
-                messageSay.content = messageSay.content.toLowerCase().replace("sb say ", "");
-                message.channel.send(messageSay);
+                message.delete();
+                message.channel.send(messageTxt);
             },1000);
         }
         
