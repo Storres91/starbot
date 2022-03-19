f4643574839474da513e10a769ca8b7dd352579a
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-18 23:57:15 -0500

    Fixed bug for checkreminders (message too long)

--- commands/checkreminders.js
+++ commands/checkreminders.js
@@ -10,7 +10,6 @@ module.exports = {
         if (message.member.user.id == '313351494361677845' || message.member.roles.cache.some(role => role.id == staffRoleID) || message.member.roles.cache.some(role => role.id == gaManagerRoleID)){}
         else return 
 
-        var remindersMsg = `<@${message.author.id}>, These are your active reminders (No, you can't delete/edit them <a:bg_catyes:927728342331949056>)`
         let remindersData;
         try {
             remindersData = await remindersModel.find({ userID: message.author.id });
@@ -19,11 +18,13 @@ module.exports = {
         }
         if (remindersData.length == 0) return message.channel.send("Jei says you currently have no reminders. <a:cr_frogstick:887361981810626571>")
 
+        message.channel.send(`<@${message.author.id}>, These are your active reminders (No, you can't delete/edit them <a:bg_catyes:927728342331949056>)`)
+
         for(let reminder of remindersData){
             let description = reminder.description.replaceAll('*','').split("\n\n")
-            remindersMsg = remindersMsg + "\n\n<t:"+((reminder.date).toString()).slice(0,-3)+":f>\n```"+(description[0])+"```\n"+description[1]
+            message.channel.send("\n\n<t:"+((reminder.date).toString()).slice(0,-3)+":f>\n```"+(description[0])+"```\n"+description[1]) 
         }
 
-        message.channel.send(remindersMsg)
+        
     }
 }
