0da394a468ea38e9141773a6e2f5a0997a387f5c
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-04-04 00:25:15 -0500

    Added emojis to the phrases

--- commands/mychannel.js
+++ commands/mychannel.js
@@ -6,7 +6,17 @@ module.exports = {
     description: 'Displays your channel',
     aliases: ['mine', 'mych'],
     async execute(client, message, args, Discord) {
-        const PHRASES = ['Quick tp to your channel!', 'Were you looking for this?', 'I think this one belongs to you...', 'All yours!', 'Ummm...here it is?', 'FAST, GET OUT OF HERE! →', 'Let\'s get on your spaceship!', 'Lieutenant, welcome onboard!', 'Oops, you dropped your channel here...'];
+        const PHRASES = [
+            'Quick tp to your channel! <:cr_mmblush:874865113071513620>', 
+            'Were you looking for this? <:cr_kannamagnify:853385258355654687>', 
+            'I think this one belongs to you... <:cr_pikathink:856247027854999583>', 
+            'All yours! <:cr_pikacool:859169809202675762>', 
+            'Ummm...here it is? <:cr_ummthinking:889681511639568454>', 
+            'FAST, GET OUT OF HERE! <:cr_nervge:856683391214157824> →', 
+            'Let\'s get on your spaceship! <:cr_ummyayy:889681652589137920>', 
+            'Lieutenant, welcome onboard! <:cr_salutepepe:886140835329110027>', 
+            'Oops, you dropped your channel here... <:cr_dumdum:890502413956562945>'
+                    ];
         const random = Math.floor(Math.random() * PHRASES.length);
 
         if (!isAllowed({message, roles: ['CHANNEL_OWNER']})) return message.channel.send("You don't have a channel here <:cr_pepesweetsadness:851548493264322580>")
