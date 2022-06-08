0cb0d742fde2beadc9c75b48022f717ec43a8279
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-06-08 16:48:14 -0500

    Feat: Auto hide when event ends

--- events/client/erpgEvent.js
+++ events/client/erpgEvent.js
@@ -3,26 +3,64 @@ module.exports = {
     description: 'Sample description',
     async execute(client, message, Discord) {
         if(message.channel.id == '928163057430061136' || message.channel.id == '852321070022787082' ) return
+        let eventRoleId;
+
         if(message.embeds[0].fields[0].value.toLowerCase().includes('catch')){
-            message.channel.send('<@&851519388963700790>')
+            eventRoleId = '851519388963700790';
 
         } else if (message.embeds[0].fields[0].value.toLowerCase().includes('chop')){
-            message.channel.send('<@&851520347588132915>')
+            eventRoleId = '851520347588132915';
 
         } else if (message.embeds[0].fields[0].value.toLowerCase().includes('fish')){
-            message.channel.send('<@&851519530815586317>')
+            eventRoleId = '851519530815586317';
 
         } else if (message.embeds[0].fields[0].value.toLowerCase().includes('time to fight')){
-            message.channel.send('<@&851519112084455464>')
+            eventRoleId = '851519112084455464';
 
         } else if (message.embeds[0].fields[0].value.toLowerCase().includes('summon')){
-            message.channel.send('<@&851518913974894603>')
+            eventRoleId = '851518913974894603';
 
         } else if (message.embeds[0].fields[0].name.toLowerCase().includes('fight')){
-            message.channel.send('<@&854865647740387339>')
+            eventRoleId = '854865647740387339';
 
         } else if (message.embeds[0].fields[0].name.toLowerCase().includes('join')){
-            message.channel.send('<@&854865339883978772>')
+            eventRoleId = '854865339883978772';
         }
+
+        //Return true if channel is hidden (Everyone's view perm is off)
+        await message.channel.permissionOverwrites.edit(eventRoleId, {
+            VIEW_CHANNEL: true
+        })
+        message.channel.send(`<@&${eventRoleId}>`)
+
+
+        //Wait for event to end to hide again
+        const filter = (m) => m.author.id === message.author.id;
+        const eventEndCollector = new Discord.MessageCollector(message.channel, {
+            filter,
+            time: 1000 * 60
+        });
+
+        eventEndCollector.on('collect', (m)=>{
+            // If another event starts return
+            const events = ['join', 'fight', 'summon', 'time to fight', 'fish', 'chop', 'catch']
+            for (let ev of events){
+                if(m.embeds[0]?.fields[0]?.name.toLowerCase().includes(ev)) return
+            }
+
+            //End event on embed
+            if(m.embeds[0]?.fields[0]?.name.toLowerCase().includes('everyone got')) eventEndCollector.stop();
+        })
+
+        eventEndCollector.on('end', ()=>{
+            setTimeout(() => {
+                message.channel.permissionOverwrites.edit(eventRoleId, {
+                    VIEW_CHANNEL: null
+                })
+                message.channel.send('Channel is now hidden.')
+            }, 2000);
+        })
+
+
     }
 }
